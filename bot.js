4b66931662cf395a44d4f9e595a49f488b1c71c3
Sam Beckett <becketts@byu.edu>
2018-07-03 22:55:59 -0600

    generate random bs

--- bot.js
+++ bot.js
@@ -54,9 +54,70 @@ controller.hears(['[insert excuse here]'], 'ambient', function(bot, message) {
   });
 });
 
+controller.hears(['[insert jargon here]'], 'ambient', function(bot, message) {
+    bot.reply(message, rbs());
+  });
+
 function usage_tip() {
     console.log('~~~~~~~~~~');
     console.log('Execute your bot application like this:');
     console.log('clientId=<MY SLACK CLIENT ID> clientSecret=<MY CLIENT SECRET> PORT=3000 studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
     console.log('~~~~~~~~~~');
+}
+
+var verbs = [
+    "aggregate", "architect", "benchmark", "brand", "cultivate",
+    "deliver", "deploy", "disintermediate", "drive", "e-enable",
+    "embrace", "empower", "enable", "engage", "engineer",
+    "enhance", "envisioneer", "evolve", "expedite", "exploit",
+    "extend", "facilitate", "generate", "grow", "harness",
+    "implement", "incentivize", "incubate", "innovate", "integrate",
+    "iterate", "leverage", "matrix", "maximize", "mesh",
+    "monetize", "morph", "optimize", "orchestrate", "productize",
+    "recontextualize", "redefine", "reintermediate", "reinvent", "repurpose",
+    "revolutionize", "scale", "seize", "strategize", "streamline",
+    "syndicate", "synergize", "synthesize", "target", "transform",
+    "transition", "unleash", "utilize", "visualize", "whiteboard"
+]
+var adjectives = [
+    "24/365", "24/7", "B2B", "B2C",
+    "back-end", "best-of-breed", "bleeding-edge", "bricks-and-clicks",
+    "clicks-and-mortar", "collaborative", "compelling", "cross-platform",
+    "cross-media", "customized", "cutting-edge", "distributed",
+    "dot-com", "dynamic", "e-business", "efficient",
+    "end-to-end", "enterprise", "extensible", "frictionless",
+    "front-end", "global", "granular", "holistic",
+    "impactful", "innovative", "integrated", "interactive",
+    "intuitive", "killer", "leading-edge", "magnetic",
+    "mission-critical", "next-generation", "one-to-one", "open-source",
+    "out-of-the-box", "plug-and-play", "proactive", "real-time",
+    "revolutionary", "rich", "robust", "scalable",
+    "seamless", "sexy", "sticky", "strategic",
+    "synergistic", "transparent", "turn-key", "ubiquitous",
+    "user-centric", "value-added", "vertical", "viral",
+    "virtual", "visionary", "web-enabled", "wireless", "world-class"
+]
+var nouns = [
+    "action-items", "applications", "architectures", "bandwidth",
+    "channels", "communities", "content", "convergence",
+    "deliverables", "e-business", "e-commerce", "e-markets",
+    "e-services", "e-tailers", "experiences", "eyeballs",
+    "functionalities", "infomediaries", "infrastructures", "initiatives",
+    "interfaces", "markets", "methodologies", "metrics",
+    "mindshare", "models", "networks", "niches",
+    "paradigms", "partnerships", "platforms", "portals",
+    "relationships", "ROI", "synergies", "web-readiness",
+    "schemas", "solutions", "supply-chains", "systems",
+    "technologies", "users", "vortals", "web services"
+]
+
+function randomInt(low, high){
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+function rbs() {
+    var bs = verbs[randomInt(0, verbs.length - 1)];
+    bs += ' ' + adjectives[randomInt(0, adjectives.length - 1)];
+    bs += ' ' + nouns[randomInt(0, nouns.length - 1)];
+    return bs;
 }
